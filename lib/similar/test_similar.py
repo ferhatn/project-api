69ffa35750e4f99dfdee02b6dd4288b7c5d5b104
Admin <ferhat.nouis@gmail.com>
2021-11-18 14:19:25 +0000

    update message

---
+++ lib/similar/test_similar.py
@@ -0,0 +1,13 @@
+import similar 
+
+example = [
+    ("User1", "FilmA"), 
+    ("User1", "FilmB"), 
+    ("User2", "FilmC"), 
+    ("User2", "FilmD"),
+    ("User3", "FilmE")
+]
+
+
+def test_sampledataset():
+    assert len(example) == 5
